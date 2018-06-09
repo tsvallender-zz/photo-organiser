ddfa2b3b7f594dc1377a9d510974aac48870a5e7
Trevor Vallender <tsv@tsv.me.uk>
2018-06-09 13:05:13 +0100

    Initial work, prints files dates

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
+from pathlib import Path
 from PIL import Image
 from PIL.ExifTags import TAGS
+from re import split
 
-def get_exif(f):
-    r = {}
-    i = Image.open(f)
-    exif = i.getexif()
-    for tag, value in exif.items():
-        decoded = TAGS.get(tag, tag)
-        r[decoded] = value
-    return r
+def traverse_files(p):
+    for f in p.rglob('*.jpg'):
+        print(f.name)
+        i = Image.open(f)
+        data = i._getexif()
+        for tag, value in data.items():
+            if TAGS.get(tag, tag) == "DateTime":
+                dt = value
+                break
+        print(split(':| ', dt))
 
+traverse_files(Path.home() / 'Photos')
 
