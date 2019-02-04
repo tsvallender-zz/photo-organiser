bc00d694f22ae6518ec18c5f89979e82fc46a05d
Trevor Vallender <tsv@tsv.me.uk>
2019-02-04 20:41:44 +0000

    Restructured and improved

--- main.py
+++ main.py
@@ -1,29 +1,78 @@
 #! /usr/bin/env python3
 
+import argparse
 from pathlib import Path
 from PIL import Image
 from PIL.ExifTags import TAGS
 from re import split
+import shutil
 
-def traverse_files(p):
-    for f in p.rglob('*.jpg'):
-        print(f.name)
-        i = Image.open(f)
-        data = i._getexif()
-        for tag, value in data.items():
-            if TAGS.get(tag, tag) == "DateTime":
-                dt = value
-                break
-        sort_photo(f, split(':| ', dt))
+def main():
+    parser = argparse.ArgumentParser(description="Sort photos by date.")
+    parser.add_argument('-i', '--input',
+                        help="Input folder")
+    parser.add_argument('-o', '--output',
+                        help="Output folder")
+    parser.add_argument('-d', '--duplicates',
+                        help="Detect and delete duplicates",
+                        action="store_true")
+    parser.add_argument('-f', '--folder',
+                        help="Folder structure for output")
+    parser.add_argument('-v', '--verbose',
+                        help="Verbose mode",
+                        action="store_true")
+    parser.add_argument('-n', '--noaction',
+                        help="Don't perform any action",
+                        action="store_true")
+    parser.add_argument('-r', "--remove",
+                        help="Delete original file",
+                        action="store_true")
+    args = parser.parse_args()
+    photosort = PhotoSort(args)
+    photosort.run()
 
-def sort_photo(p, dt):
-    year, month, day, hour, minute, second = dt
-    root = Path('/home/tsv/Photos/Sorted')
-    f = hour+minute+second+".jpg"
-    new_path = root / year / month / day
-    print(new_path)
-    Path.mkdir(new_path, parents=True, exist_ok=True)
-    p.rename(new_path / f)
+class PhotoSort(object):
+    def __init__(self, flags):
+        self.flags = flags
 
-traverse_files(Path.home() / 'Photos')
+    def run(self):
+        if self.flags.input:
+            self.traverse_files(Path(self.flags.input))
+        else:
+            print("No input specified")
+            exit
+
+    def traverse_files(self, p):
+        for f in p.rglob('*.jpg'):
+            i = Image.open(f)
+            data = i._getexif()
+            for tag, value in data.items():
+                if TAGS.get(tag, tag) == "DateTime":
+                    dt = value
+                    break
+            self.sort_photo(f, split(':| ', dt))
+
+    def sort_photo(self, p, dt):
+        year, month, day, hour, minute, second = dt
+
+        if self.flags.output:
+            # This needs to be checked
+            new_path = Path(self.flags.output)
+        else:
+            new_path = Path.home()
+        new_path = new_path / year / month / day
+
+        f = hour+minute+second+".jpg"
+
+        if self.flags.verbose:
+            print("Moving to image to " + str(new_path) + '/' + f)
+        if not self.flags.noaction:
+            Path.mkdir(new_path, parents=True, exist_ok=True)
+            if self.flags.remove:
+                p.rename(new_path / f)
+            else:
+                shutil.copyfile(str(p), str(new_path) +'/' + f) 
+                
+if __name__ == "__main__":
+    main()
 
